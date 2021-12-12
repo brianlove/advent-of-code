0e173ead715a29993c8f6191b1b3a5e29b02e309
Brian Love <brianlove128@gmail.com>
2021-12-12 18:28:27 -0500

    Day 12, part 2 - Passage Pathing
    Add Part 2 of Day 12
    
    Yeah, the code's kinda a mess and probably isn't elegant.  That's not
    the focus of this project. :)

--- day-12/day12.py
+++ day-12/day12.py
@@ -1,11 +1,20 @@
+#! /usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('filename', help='The input file')
+parser.add_argument('-d', '--double-back', action='store_true', help='Can we double back?')
+
+args = parser.parse_args()
 
 #filename = 'demo-12.txt'
 #filename = 'demo-12c.txt'
-filename = 'input-12.txt'
+#filename = 'input-12.txt'
 
 DEBUG = False
 
-with open(filename) as file:
+with open(args.filename) as file:
     lines = file.readlines()
     lines = [line.rstrip().split('-') for line in lines]
 
@@ -23,15 +32,16 @@ for start, end in lines:
         doors[end] = [start]
 
 
-if DEBUG:
-    for cave in doors:
+for cave in doors:
+    doors[cave].sort()
+    if DEBUG:
         print(f"{cave} -> {doors[cave]}")
-    print()
-    print()
+print()
+print()
 
 
 # return: a full path to the end, if possible, otherwise false??
-def check_path(room, so_far, indent):
+def check_path(room, so_far, indent, has_doubled_back):
     this_path = so_far[:]
     this_path.append(room)
     if DEBUG:
@@ -39,20 +49,35 @@ def check_path(room, so_far, indent):
 
     solutions = []
     for dest in doors[room]:
+        repeat_small_cave = dest[0].islower() and dest in so_far
+
+
         if dest == 'end':
+            # Found the path out; record the solution
             solution = this_path[:]
             solution.append(dest)
             solutions.append(solution)
             if DEBUG:
                 print(f"{'  '*indent}- found solution '{dest}', Path: {solution}")
-        elif dest[0].islower() and dest in so_far:
+        elif repeat_small_cave and (not args.double_back or has_doubled_back or dest == 'start'):
+            # Part 1:
+            #   repeat small cave
+            # Part 2:
+            #   repeat small cave, and we have either already doubled back or it is 'start'
+
+            # We've already met the max number of times that we can visit this cave
             if DEBUG:
                 print(f"{'  '*indent}- '{dest}' = dup")
             continue
         else:
+            # Keep searching
             if DEBUG:
                 print(f"{'  '*indent}- '{dest}' = new cave")
-            next_solutions = check_path(dest, this_path, indent+1)
+
+            double_back_eligible = repeat_small_cave and dest != 'start'
+            will_double_back = has_doubled_back or (double_back_eligible and args.double_back)
+
+            next_solutions = check_path(dest, this_path, indent+1, will_double_back)
             if len(next_solutions):
                 solutions += next_solutions
 
@@ -60,10 +85,9 @@ def check_path(room, so_far, indent):
         print(f"{'  '*indent} Room '{room}' :: {solutions}")
     return solutions
 
-result = check_path('start', [], 0)
+result = check_path('start', [], 0, False)
 
 print()
 print(f"Part 1 result: {len(result)} paths found")
 #for entry in result:
 #    print(entry)
-
