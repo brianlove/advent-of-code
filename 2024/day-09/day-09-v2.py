ea532b160939a98cfdbb86e20eabf1c0f92ae37b
Brian Love <22353962+brianlove@users.noreply.github.com>
2024-12-10 01:37:42 -0500

    Day 9, part 2
    Part 2 v2 originally made in separate file (distinct from the
    `part2_bad` function), but then transfered to the main file on
    committing.

--- 2024/day-09/day-09-v2.py
+++ 2024/day-09/day-09-v2.py
@@ -3,132 +3,13 @@
 import argparse
 import collections
 
-parser = argparse.ArgumentParser("Day 9")
+parser = argparse.ArgumentParser("Day 9, v2")
 parser.add_argument("infile", help="The input file")
 args = parser.parse_args()
 
 def part1(lines):
     print("\n\n~~ PART 1~~")
-    rawInput = list(int(x) for x in lines[0])
-
-    fileIdsToSizes = {}
-    filesystem = []
-    for ix, digit in enumerate(rawInput):
-        if ix % 2 == 0: ## looking at a file
-            fileId = ix // 2
-            fileIdsToSizes[fileId] = digit
-            filesystem.extend([fileId] * digit)
-        else:
-            filesystem.extend([None] * digit)
-
-    fillingIndex = 0
-    compactedFilesystem = list(filesystem)
-    while None in compactedFilesystem:
-        lastEntry = compactedFilesystem.pop()
-        if lastEntry == None:
-            continue
-        while compactedFilesystem[fillingIndex] is not None:
-            fillingIndex += 1
-        compactedFilesystem[fillingIndex] = lastEntry
-    # print("compacted>", compactedFilesystem)
-
-    checksum = sum(ix * digit for ix, digit in enumerate(compactedFilesystem))
-    print("checksum:", checksum)
-
-
-def flatten_list(forest):
-    return [leaf for tree in forest for leaf in tree]
-
-def remapFreespace(filesystem):
-    freespaceMap = collections.defaultdict(lambda: [])
-    prevId = None
-    count = 0
-    gapStartIndex = 0
-    for ix, fileId in enumerate(filesystem):
-        if fileId is None:
-            # if prevId is not None:
-            count += 1
-        # else:
     pass
-
-def part2_bad(lines):
-    print("\n\n~~ PART 2~~")
-    rawInput = list(int(x) for x in lines[0])
-
-    fileIdsToSizes = {}
-    fileIdsToPositions = {}
-    freespaceMap = collections.defaultdict(lambda: [])
-    filesystem = []
-    filesystemIndex = 0
-    for ix, digit in enumerate(rawInput):
-        if ix % 2 == 0: ## looking at a file
-            fileId = ix // 2
-            fileIdsToSizes[fileId] = digit
-            fileIdsToPositions[fileId] = filesystemIndex
-            filesystem.extend([fileId] * digit)
-        else:
-            filesystem.extend([None] * digit)
-            ## Map from freespace sizes to where they are located
-            freespaceMap[digit].append(filesystemIndex)
-
-        filesystemIndex += digit
-
-    # print("fileIdsTOSizes:", fileIdsToSizes)
-    # print("fileIdsToPositions:", fileIdsToPositions)
-    # print("filesystem:", filesystem)
-    # print("freespaceMap:", freespaceMap)
-
-    compactedFilesystem = list(filesystem)
-
-    fileIds = list(fileIdsToSizes.keys())
-    fileIds.sort(reverse=True)
-    # print("file IDs:", fileIds)
-
-    for fileId in fileIds:
-        print("\n\nFile", fileId)
-        print("FreespaceMap:", freespaceMap)
-        availableSizes = list(freespaceMap.keys())
-        availableSizes.sort()
-        # print("available sizes:", availableSizes)
-        sizeOfFile = fileIdsToSizes[fileId]
-        availableSizesThatFit = [x for x in availableSizes if x >= sizeOfFile]
-
-        # print("gap sizes that fit:", availableSizesThatFit)
-        indicesOfWorkableGaps = flatten_list(
-            [(index, size) for index in freespaceMap[size]]
-            for size in availableSizesThatFit
-        )
-        indicesOfWorkableGaps.sort(key=lambda x: x[0])
-        # print("indices of gaps:", indicesOfWorkableGaps)
-
-        if len(indicesOfWorkableGaps) > 0:
-            gapIndex, gapSize = indicesOfWorkableGaps[0]
-            # print("- place file in index", gapIndex, "  size:", gapSize)
-
-            ## Move the file into its new position
-            for fsIndex in range(gapIndex, gapIndex + sizeOfFile):
-                compactedFilesystem[fsIndex] = fileId
-
-            ## Remove the file from its old location
-            oldPosition = fileIdsToPositions[fileId]
-            for fsIndex in range(oldPosition, oldPosition + sizeOfFile):
-                compactedFilesystem[fsIndex] = None
-            freespaceMap[sizeOfFile].append(oldPosition)
-
-            ## Record the new position and remove the old gap
-            fileIdsToPositions[fileId] = gapIndex
-            # print("freespaceMap:", freespaceMap[gapSize], "  gapIndex:", gapIndex)
-            freespaceMap[gapSize].remove(gapIndex)
-
-            ## Record any remaining space that we haven't used up
-            remainingGap = gapSize - sizeOfFile
-            if remainingGap:
-                freespaceMap[remainingGap].append(gapIndex+sizeOfFile)
-        print("CompactFS:", compactedFilesystem)
-
-    checksum = sum(ix * digit for ix, digit in enumerate(compactedFilesystem) if digit is not None)
-    print("checksum:", checksum)
-
 
 
 class FSNode:
@@ -143,7 +24,7 @@ class FSNode:
     def __str__(self):
         return f"FSNode(<{self.addr}::{self.size}>, {self.fileId})"
 
-def part2_linkedlist(lines):
+def part2(lines):
     print("\n\n~~ PART 2~~")
     rawInput = [int(x) for x in lines[0]]
     fileId = 0
@@ -256,6 +137,6 @@ def part2_linkedlist(lines):
 
 with open(args.infile) as file:
     lines = [line.rstrip() for line in file.readlines()]
-    part1(lines)
+    # part1(lines)
 
-    part2_linkedlist(lines)
+    part2(lines)
